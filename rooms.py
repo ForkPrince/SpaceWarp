f5c829207d7211ce9a40ee256d4e2d0042d32e52
Prince527 <Prince527gaming@gmail.com>
2023-07-18 15:12:19 -0300

    Update rooms.py

--- rooms.py
+++ rooms.py
@@ -19,30 +19,21 @@ class Room:
         object_below = self.objects[x // 8][y // 8 - 1]
 
         type_on = -1
-        if object_on == 17:
-            type_on = 0
-        elif object_on == 20:
-            type_on = 1
-        elif object_on == 23:
-            type_on = 2
-
         type_below = -1
-        if object_below == 17:
-            type_below = 0
-        elif object_below == 20:
-            type_below = 1
-        elif object_below == 23:
-            type_below = 2
 
-        if object_on == 17 or object_on == 20 or object_on == 23:
-            if type_on != -1 and (self.doors[type_on] == 1):
-                return 1
-        elif object_below == 17 or object_below == 20 or object_below == 23:
-            if type_below != -1 and (self.doors[type_below] == 1):
-                return 1
-        elif object_on == 13 or object_on == 14 or object_on == 15 or object_on == 16:
+        if object_on in [17, 20, 23]:
+            type_on = [17, 20, 23].index(object_on)
+
+        if object_below in [17, 20, 23]:
+            type_below = [17, 20, 23].index(object_below)
+
+        if type_on != -1 and self.doors[type_on] == 1:
+            return 1
+        elif type_below != -1 and self.doors[type_below] == 1:
+            return 1
+        elif object_on in [13, 14, 15, 16]:
             return 2
-        elif object_on == 26 or object_on == 27:
+        elif object_on in [26, 27]:
             return 3
         elif 1 <= object_on <= 12:
             return 1
@@ -56,53 +47,31 @@ class Room:
 
         for x, line in enumerate(self.objects):
             for y, tile in enumerate(line):
-                if tile == 17:
-                    if self.keys[0] == 0 or self.button_state[0] != 0:
-                        self.doors[0] = 0
+                if tile in [17, 20, 23]:
+                    door_index = [17, 20, 23].index(tile)
+                    if self.keys[door_index] == 0 or self.button_state[door_index] != 0:
+                        self.doors[door_index] = 0
                     else:
-                        self.doors[0] = 1
-                elif tile == 18:
-                    if x * 8 - 4 <= player_x <= x * 8 + 4 and y * 8 == player_y:
-                        self.button_state[0] = 150
-                    elif x * 8 - 5 <= player_x <= x * 8 + 5 and y * 8 - 1 <= player_y <= y * 8 and self.button_state[0] <= 2:
-                        self.button_state[0] = 2
-                    elif x * 8 - 6 <= player_x <= x * 8 + 6 and y * 8 - 2 < player_y <= y * 8 and self.button_state[0] <= 1:
-                        self.button_state[0] = 1
-                elif tile == 19:
-                    if (not(player_x + 7 < x * 8 or x * 8 + 7 < player_x or player_y + 7 < y * 8 or y * 8 + 7 < player_y)):
-                        self.keys[0] = 0
+                        self.doors[door_index] = 1
+                elif tile in [18, 21, 24]:
+                    button_index = [18, 21, 24].index(tile)
+                    button_x = x * 8
+                    button_y = y * 8
 
-                elif tile == 20:
-                    if self.keys[1] == 0 or self.button_state[1] != 0:
-                        self.doors[1] = 0
-                    else:
-                        self.doors[1] = 1
-                elif tile == 21:
-                    if x * 8 - 4 <= player_x <= x * 8 + 4 and y * 8 == player_y:
-                        self.button_state[1] = 150
-                    elif x * 8 - 5 <= player_x <= x * 8 + 5 and y * 8 - 1 <= player_y <= y * 8 and self.button_state[1] <= 2:
-                        self.button_state[1] = 2
-                    elif x * 8 - 6 <= player_x <= x * 8 + 6 and y * 8 - 2 < player_y <= y * 8 and self.button_state[1] <= 1:
-                        self.button_state[1] = 1
-                elif tile == 22:
-                    if (not(player_x + 7 < x * 8 or x * 8 + 7 < player_x or player_y + 7 < y * 8 or y * 8 + 7 < player_y)):
-                        self.keys[1] = 0
+                    if button_x - 4 <= player_x <= button_x + 4 and button_y == player_y:
+                        self.button_state[button_index] = 150
+                    elif button_x - 5 <= player_x <= button_x + 5 and button_y - 1 <= player_y <= button_y and self.button_state[button_index] <= 2:
+                        self.button_state[button_index] = 2
+                    elif button_x - 6 <= player_x <= button_x + 6 and button_y - 2 < player_y <= button_y and self.button_state[button_index] <= 1:
+                        self.button_state[button_index] = 1
 
-                elif tile == 23:
-                    if self.keys[2] == 0 or self.button_state[2] != 0:
-                        self.doors[2] = 0
-                    else:
-                        self.doors[2] = 1
-                elif tile == 24:
-                    if x * 8 - 4 <= player_x <= x * 8 + 4 and y * 8 == player_y:
-                        self.button_state[2] = 150
-                    elif x * 8 - 5 <= player_x <= x * 8 + 5 and y * 8 - 1 <= player_y <= y * 8 and self.button_state[2] <= 2:
-                        self.button_state[2] = 2
-                    elif x * 8 - 6 <= player_x <= x * 8 + 6 and y * 8 - 2 < player_y <= y * 8 and self.button_state[2] <= 1:
-                        self.button_state[2] = 1
-                elif tile == 25:
-                    if (not(player_x + 7 < x * 8 or x * 8 + 7 < player_x or player_y + 7 < y * 8 or y * 8 + 7 < player_y)):
-                        self.keys[2] = 0
+                elif tile in [19, 22, 25]:
+                    key_index = [19, 22, 25].index(tile)
+                    key_x = x * 8
+                    key_y = y * 8
+
+                    if not (player_x + 7 < key_x or key_x + 7 < player_x or player_y + 7 < key_y or key_y + 7 < player_y):
+                        self.keys[key_index] = 0
 
         for i in range(3):
             if self.doors[i] == 0 and self.doors_state[i] > 0:
@@ -113,85 +82,64 @@ class Room:
     def draw_room(self):
         for x, line in enumerate(self.objects):
             for y, tile in enumerate(line):
-                if tile == 1:
-                    pyxel.blt(x * 8, y * 8, 0, 48, 8, 8, 8, 0)
-                elif tile == 2:
-                    pyxel.blt(x * 8, y * 8, 0, 48, 0, 8, 8, 0)
-                elif tile == 3:
-                    pyxel.blt(x * 8, y * 8, 0, 56, 0, 8, 8, 0)
-                elif tile == 4:
-                    pyxel.blt(x * 8, y * 8, 0, 56, 8, 8, 8, 0)
-                elif tile == 5:
-                    pyxel.blt(x * 8, y * 8, 0, 16, 16, 8, 8, 0)
-                elif tile == 6:
-                    pyxel.blt(x * 8, y * 8, 0, 24, 16, 8, 8, 0)
-                elif tile == 7:
-                    pyxel.blt(x * 8, y * 8, 0, 16, 24, 8, 8, 0)
-                elif tile == 8:
-                    pyxel.blt(x * 8, y * 8, 0, 24, 24, 8, 8, 0)
-                elif tile == 9:
-                    pyxel.blt(x * 8, y * 8, 0, 32, 16, 8, 8, 0)
-                elif tile == 10:
-                    pyxel.blt(x * 8, y * 8, 0, 40, 16, 8, 8, 0)
-                elif tile == 11:
-                    pyxel.blt(x * 8, y * 8, 0, 32, 24, 8, 8, 0)
-                elif tile == 12:
-                    pyxel.blt(x * 8, y * 8, 0, 40, 24, 8, 8, 0)
+                tile_mapping = {
+                    1: (48, 8),
+                    2: (48, 0),
+                    3: (56, 0),
+                    4: (56, 8),
+                    5: (16, 16),
+                    6: (24, 16),
+                    7: (16, 24),
+                    8: (24, 24),
+                    9: (32, 16),
+                    10: (40, 16),
+                    11: (32, 24),
+                    12: (40, 24),
+                    13: (0, 16),
+                    14: (8, 16),
+                    15: (0, 24),
+                    16: (8, 24)
+                }
+                door_mapping = {
+                    17: [(32, 32), (32, 40)],
+                    20: [(40, 32), (40, 40)],
+                    23: [(48, 32), (48, 40)]
+                }
+                button_mapping = {
+                    18: (24, 40),
+                    21: (16, 32),
+                    24: (16, 40)
+                }
+                key_mapping = {
+                    19: (56, 32),
+                    22: (56, 40),
+                    25: (56, 48)
+                }
 
-                elif tile == 13:
-                    pyxel.blt(x * 8, y * 8, 0, 0, 16, 8, 8, 0)
-                elif tile == 14:
-                    pyxel.blt(x * 8, y * 8, 0, 8, 16, 8, 8, 0)
-                elif tile == 15:
-                    pyxel.blt(x * 8, y * 8, 0, 0, 24, 8, 8, 0)
-                elif tile == 16:
-                    pyxel.blt(x * 8, y * 8, 0, 8, 24, 8, 8, 0)
+                if tile in tile_mapping:
+                    pyxel.blt(x * 8, y * 8, 0, *tile_mapping[tile], 8, 8, 0)
 
-                elif tile == 17:
-                    pyxel.blt(x * 8, y * 8 + self.doors_state[0] - 8, 0, 32, 32, 8, 8, 0)
-                    pyxel.blt(x * 8, y * 8 + 16 - self.doors_state[0], 0, 32, 40, 8, 8, 0)
-                elif tile == 18:
-                    if self.button_state[0] == 0:
-                        pyxel.blt(x * 8, y * 8, 0, 24, 40, 8, 8, 0)
-                    elif self.button_state[0] == 1:
-                        pyxel.blt(x * 8, y * 8 + 1, 0, 24, 40, 8, 7, 0)
-                    elif self.button_state[0] == 2:
-                        pyxel.blt(x * 8, y * 8 + 2, 0, 24, 40, 8, 6, 0)
-                elif tile == 19:
-                    if self.keys[0] == 1:
-                        pyxel.blt(x * 8, y * 8, 0, 56, 32, 8, 8, 0)
-                    else:
-                        pyxel.blt(x * 8, y * 8, 0, 0, 0, 8, 8, 0)
+                elif tile in door_mapping:
+                    door_index = [17, 20, 23].index(tile)
 
-                elif tile == 20:
-                    pyxel.blt(x * 8, y * 8 + self.doors_state[1] - 8, 0, 40, 32, 8, 8, 0)
-                    pyxel.blt(x * 8, y * 8 + 16 - self.doors_state[1], 0, 40, 40, 8, 8, 0)
-                elif tile == 21:
-                    if self.button_state[1] == 0:
-                        pyxel.blt(x * 8, y * 8, 0, 16, 32, 8, 8, 0)
-                    elif self.button_state[1] == 1:
-                        pyxel.blt(x * 8, y * 8 + 1, 0, 16, 32, 8, 7, 0)
-                    elif self.button_state[1] == 2:
-                        pyxel.blt(x * 8, y * 8 + 2, 0, 16, 32, 8, 6, 0)
-                elif tile == 22:
-                    if self.keys[1] == 1:
-                        pyxel.blt(x * 8, y * 8, 0, 56, 40, 8, 8, 0)
-                    else:
-                        pyxel.blt(x * 8, y * 8, 0, 0, 0, 8, 8, 0)
+                    pyxel.blt(x * 8, y * 8 + self.doors_state[door_index] - 8, 0, *door_mapping[tile][0], 8, 8, 0)
+                    pyxel.blt(x * 8, y * 8 + 16 - self.doors_state[door_index], 0, *door_mapping[tile][1], 8, 8, 0)
 
-                elif tile == 23:
-                    pyxel.blt(x * 8, y * 8 + self.doors_state[2] - 8, 0, 48, 32, 8, 8, 0)
-                    pyxel.blt(x * 8, y * 8 + 16 - self.doors_state[2], 0, 48, 40, 8, 8, 0)
-                elif tile == 24:
-                    if self.button_state[2] == 0:
-                        pyxel.blt(x * 8, y * 8, 0, 16, 40, 8, 8, 0)
-                    elif self.button_state[2] == 1:
-                        pyxel.blt(x * 8, y * 8 + 1, 0, 16, 40, 8, 7, 0)
-                    elif self.button_state[2] == 2:
-                        pyxel.blt(x * 8, y * 8 + 2, 0, 16, 40, 8, 6, 0)
-                elif tile == 25:
-                    if self.keys[2] == 1:
-                        pyxel.blt(x * 8, y * 8, 0, 56, 48, 8, 8, 0)
+                elif tile in button_mapping:
+                    button_index = [18, 21, 24].index(tile)
+
+                    if self.button_state[button_index] == 0:
+                        pyxel.blt(x * 8, y * 8, 0, *button_mapping[tile], 8, 8, 0)
+                    elif self.button_state[button_index] == 1:
+                        pyxel.blt(x * 8, y * 8 + 1, 0, *button_mapping[tile], 8, 7, 0)
+                    elif self.button_state[button_index] == 2:
+                        pyxel.blt(x * 8, y * 8 + self.button_state[button_index], 0, *button_mapping[tile], 8, 6, 0)
+
+                elif tile in key_mapping:
+                    key_index = [19, 22, 25].index(tile)
+
+                    if self.keys[key_index] == 1:
+                        pyxel.blt(x * 8, y * 8, 0, *key_mapping[tile], 8, 8, 0)
                     else:
                         pyxel.blt(x * 8, y * 8, 0, 0, 0, 8, 8, 0)
 
