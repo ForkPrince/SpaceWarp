b0e405b86f923f03a4db39d091edb2aa80e0f726
Prince527 <Prince527gaming@gmail.com>
2024-04-27 15:09:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,210 @@
+import pyxel, json, copy, math
+from player import Player
+from rooms import Room
+from menu import Menu
+from pygame import mixer
+
+mixer.music.load("ressources/bgm.mp3")
+
+def round_half_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n*multiplier + 0.5) / multiplier
+
+import numpy as np
+
+def rotate(array):
+    array_np = np.array(array)
+
+    rotated = np.rot90(array_np, k=0, axes=(0,1))
+
+    return rotated.tolist()
+
+class App:
+    def __init__(self):
+        self.gamestate = 0
+        self.menu = Menu()
+        mixer.music.play()
+
+        pyxel.init(128, 128, title='SpaceWarp')
+        pyxel.load("ressources/assets.pyxres")
+        pyxel.run(self.update, self.draw)
+        
+    def load_mask(self):
+        tilemap = pyxel.tilemaps[self.difficulty + 1]
+
+        tile_mapping = {
+            (6, 1): 1,
+            (6, 0): 2,
+            (7, 0): 3,
+            (7, 1): 4,
+            (2, 2): 5,
+            (3, 2): 6,
+            (2, 3): 7,
+            (3, 3): 8,
+            (4, 2): 9,
+            (5, 2): 10,
+            (4, 3): 11,
+            (5, 3): 12,
+            (0, 4): 26,
+            (0, 5): 27,
+            (1, 4): 28,
+            (1, 5): 29,
+            (3, 5): 18,
+            (2, 4): 21,
+            (2, 5): 24,
+            (7, 4): 19,
+            (7, 5): 22,
+            (7, 6): 25,
+            (4, 5): 0,
+            (5, 5): 0,
+            (6, 5): 0,
+            (4, 4): 17,
+            (5, 4): 20,
+            (6, 4): 23,
+            (0, 2): 13,
+            (0, 3): 15,
+            (1, 2): 14,
+            (1, 3): 16,
+            (6, 2): 30,
+            (6, 3): 31,
+            (7, 2): 32,
+            (7, 3): 33,
+            (4, 0): 34,
+            (4, 1): 35,
+            (5, 0): 36,
+            (5, 1): 37,
+            (0, 1): 38,
+            (3, 4): 39,
+            (0, 6): 40,
+            (0, 7): 41,
+            (1, 6): 42,
+            (1, 7): 43,
+            (2, 6): 44,
+            (2, 7): 45,
+            (3, 6): 46,
+            (3, 7): 47,
+            (4, 6): 48,
+            (4, 7): 49,
+            (5, 6): 50,
+            (5, 7): 51,
+            (6, 6): 52,
+            (6, 7): 53,
+            (7, 6): 54,
+            (7, 7): 55,
+            (8, 7): 56
+        }
+
+        self.rooms = [[0] * 16] * 16
+
+        for level in range(16):
+            room = [[0] * 16 for _ in range(16)]
+
+            offset = 16 * level
+
+            for x in range(16):
+                for y in range(16):
+                    tile = tilemap.pget(x + offset, y)
+
+                    tile_type = 0 if tile == (0, 0) else 2
+
+                    if tile in tile_mapping:
+                        tile_index = tile_mapping[tile]
+
+                        tile_type = tile_index
+
+                    room[x][y] = tile_type
+
+            room = rotate(room)
+            self.rooms[level] = Room(room)
+
+    def update(self):
+        if self.gamestate == 0:
+            self.gamestate = self.menu.update_menu()
+            if self.gamestate == 1:
+                self.difficulty = self.menu.difficulty
+                self.load_mask()
+                self.rooms[0].spawn_x, self.rooms[0].spawn_y = 0, 112
+                self.enter_room_state = copy.deepcopy(self.rooms[0])
+                self.start_frame = pyxel.frame_count
+                self.end_frame = 0
+                self.player = Player(0, 112, 0)
+                self.current_screen = 0
+            return
+        if self.gamestate == 2:
+            if pyxel.btnp(pyxel.KEY_RETURN) or pyxel.btnp(pyxel.KEY_Z) or pyxel.btnr(pyxel.GAMEPAD1_BUTTON_A):
+                self.gamestate = 0
+            return
+        self.player.move(self.rooms[self.current_screen], self.current_screen, self.difficulty)
+        self.update_screen_position()
+        self.rooms[self.current_screen].update_room(self.player.x, self.player.y)
+        
+        if self.player.alive == 0 or pyxel.btnp(pyxel.KEY_R) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_Y):
+            self.player.reset(self.rooms[self.current_screen].spawn_x, self.rooms[self.current_screen].spawn_y)
+            self.player.alive = 1
+            self.rooms[self.current_screen] = copy.deepcopy(self.enter_room_state)
+        
+        if self.player.win == 1:
+            self.gamestate = 2
+            self.end_frame = pyxel.frame_count
+            self.total_time = str(round_half_up((self.end_frame - self.start_frame)/30, 2))
+            self.end_anim_frame = 0
+
+    def update_screen_position(self):
+        if self.player.x == 124:
+            self.current_screen += 1
+            self.player.x -= 128
+            self.rooms[self.current_screen].spawn_x = self.player.x + 4
+            self.rooms[self.current_screen].spawn_y = self.player.y
+            self.enter_room_state = copy.deepcopy(self.rooms[self.current_screen])
+        elif self.player.x == -5 and self.current_screen != 0:
+            self.current_screen -= 1
+            self.player.x += 128
+            self.rooms[self.current_screen].spawn_x = self.player.x - 4
+            self.rooms[self.current_screen].spawn_y = self.player.y
+            self.enter_room_state = copy.deepcopy(self.rooms[self.current_screen])
+
+
+    def draw(self):
+        if not mixer.music.get_busy():
+            mixer.music.play()
+        if self.gamestate == 0:
+            pyxel.cls(0)
+            self.menu.draw_menu()
+        elif self.gamestate == 1:
+            pyxel.cls(0)
+            self.rooms[self.current_screen].draw_room()
+            self.player.draw_player()
+
+            #! Debug Mode
+            if self.menu.debug == 1 and (pyxel.btnp(pyxel.KEY_LEFTBRACKET) or pyxel.btnp(pyxel.KEY_RIGHTBRACKET)):
+                if pyxel.btnp(pyxel.KEY_LEFTBRACKET):
+                    self.current_screen = (self.current_screen - 1) % len(self.rooms)
+                if pyxel.btnp(pyxel.KEY_RIGHTBRACKET):
+                    self.current_screen = (self.current_screen + 1) % len(self.rooms)
+
+                if self.current_screen == 1 or self.current_screen == 3:
+                    self.rooms[self.current_screen].spawn_x = 0
+                    self.rooms[self.current_screen].spawn_y = 112
+                elif self.current_screen == 2:
+                    self.rooms[self.current_screen].spawn_x = 0
+                    self.rooms[self.current_screen].spawn_y = 48
+
+                self.enter_room_state = copy.deepcopy(self.rooms[self.current_screen])
+                self.player.alive = 0
+
+        elif self.gamestate == 2:
+            pyxel.cls(0)
+            if self.end_anim_frame < 72:
+                pyxel.bltm(0, 0, 0, 128, 0, 128, 64)
+                pyxel.blt(88, 48 - self.end_anim_frame, 0, 0, 32, 16, 16)
+                pyxel.blt(92, 64 - self.end_anim_frame, 0, 8, 16, 8, 8)
+                pyxel.bltm(0, 64, 0, 128, 64, 128, 64)
+                self.end_anim_frame += 1        
+            else:
+                pyxel.bltm(0, 0, 0, 0, 0, 128, 128)
+                pyxel.text(48, 48, "You win!", 7)
+                pyxel.text(40, 56, "Time: " + self.total_time + "s", 7)
+                pyxel.text(42, 72, "Difficulty:", 7)
+                pyxel.text(48, 80, str(self.menu.options[1][self.difficulty]), 0)
+
+App()
---
+++ rooms.py
@@ -0,0 +1,175 @@
+import pyxel
+
+class Room:
+    def __init__(self, objects):
+        self.objects = objects
+        self.types = []
+        self.keys = [1] * 3
+        self.doors = [1] * 3
+        self.doors_state = [8] * 3
+        self.button_state = [0] * 3
+        self.spawn_x = 0
+        self.spawn_y = 0
+
+    def collision(self, x, y):
+        if x // 8 > 15 or y // 8 > 15:
+            return 0
+
+        object_on = self.objects[x // 8][y // 8]
+        object_below = self.objects[x // 8][y // 8 - 1]
+
+        type_on = -1
+        type_below = -1
+
+        if object_on in [17, 20, 23]:
+            type_on = [17, 20, 23].index(object_on)
+
+        if object_below in [17, 20, 23]:
+            type_below = [17, 20, 23].index(object_below)
+
+        if type_on != -1 and self.doors[type_on] == 1:
+            return 1
+        elif type_below != -1 and self.doors[type_below] == 1:
+            return 1
+        elif object_on in [13, 14, 15, 16]:
+            return 2
+        elif object_on in [26, 27, 28, 29]:
+            return 3
+        elif 1 <= object_on <= 12:
+            return 1
+        else:
+            return 0
+
+    def update_room(self, player_x, player_y):
+        for i in range(3):
+            if self.button_state[i] != 0:
+                self.button_state[i] -= 1
+
+        for x, line in enumerate(self.objects):
+            for y, tile in enumerate(line):
+                if tile in [17, 20, 23]:
+                    door_index = [17, 20, 23].index(tile)
+                    if self.keys[door_index] == 0 or self.button_state[door_index] != 0:
+                        self.doors[door_index] = 0
+                    else:
+                        self.doors[door_index] = 1
+                elif tile in [18, 21, 24]:
+                    button_index = [18, 21, 24].index(tile)
+                    button_x = x * 8
+                    button_y = y * 8
+
+                    if button_x - 4 <= player_x <= button_x + 4 and button_y == player_y:
+                        self.button_state[button_index] = 150
+                    elif button_x - 5 <= player_x <= button_x + 5 and button_y - 1 <= player_y <= button_y and self.button_state[button_index] <= 2:
+                        self.button_state[button_index] = 2
+                    elif button_x - 6 <= player_x <= button_x + 6 and button_y - 2 < player_y <= button_y and self.button_state[button_index] <= 1:
+                        self.button_state[button_index] = 1
+
+                elif tile in [19, 22, 25]:
+                    key_index = [19, 22, 25].index(tile)
+                    key_x = x * 8
+                    key_y = y * 8
+
+                    if not (player_x + 7 < key_x or key_x + 7 < player_x or player_y + 7 < key_y or key_y + 7 < player_y):
+                        self.keys[key_index] = 0
+
+        for i in range(3):
+            if self.doors[i] == 0 and self.doors_state[i] > 0:
+                self.doors_state[i] -= 1
+            elif self.doors[i] == 1 and self.doors_state[i] < 8:
+                self.doors_state[i] += 1
+
+    def draw_room(self):
+        for x, line in enumerate(self.objects):
+            for y, tile in enumerate(line):
+                tile_mapping = {
+                    1: (48, 8),
+                    2: (48, 0),
+                    3: (56, 0),
+                    4: (56, 8),
+                    5: (16, 16),
+                    6: (24, 16),
+                    7: (16, 24),
+                    8: (24, 24),
+                    9: (32, 16),
+                    10: (40, 16),
+                    11: (32, 24),
+                    12: (40, 24),
+                    13: (0, 16),
+                    14: (8, 16),
+                    15: (0, 24),
+                    16: (8, 24),
+                    26: (0, 32),
+                    27: (0, 40),
+                    28: (8, 32),
+                    29: (8, 40),
+                    30: (48, 16),
+                    31: (48, 24),
+                    32: (56, 16),
+                    33: (56, 24),
+                    34: (32, 0),
+                    35: (32, 16),
+                    36: (40, 0),
+                    37: (40, 16),
+                    38: (0, 8),
+                    39: (24, 32),
+                    40: (0, 48),
+                    41: (0, 56),
+                    42: (8, 48),
+                    43: (8, 56),
+                    44: (16, 48),
+                    45: (16, 56),
+                    46: (24, 48),
+                    47: (24, 56),
+                    48: (32, 48),
+                    49: (32, 56),
+                    50: (40, 48),
+                    51: (40, 56),
+                    52: (48, 48),
+                    53: (48, 56),
+                    54: (56, 48),
+                    55: (56, 56),
+                    56: (64, 56)
+                }
+                door_mapping = {
+                    17: [(32, 32), (32, 40)],
+                    20: [(40, 32), (40, 40)],
+                    23: [(48, 32), (48, 40)]
+                }
+                button_mapping = {
+                    18: (24, 40),
+                    21: (16, 32),
+                    24: (16, 40)
+                }
+                key_mapping = {
+                    19: (56, 32),
+                    22: (56, 40),
+                    25: (56, 48)
+                }
+
+                if tile in tile_mapping:
+                    pyxel.blt(x * 8, y * 8, 0, *tile_mapping[tile], 8, 8, 0)
+
+                elif tile in door_mapping:
+                    door_index = [17, 20, 23].index(tile)
+
+                    pyxel.blt(x * 8, y * 8, 0, 32 + 8 * (door_index), 40 - self.doors_state[door_index], 8, self.doors_state[door_index], 0)
+                    pyxel.blt(x * 8, y * 8 + 16 - self.doors_state[door_index], 0, 32 + 8 * (door_index), 40, 8, self.doors_state[door_index], 0)
+
+                elif tile in button_mapping:
+                    button_index = [18, 21, 24].index(tile)
+
+                    if self.button_state[button_index] == 0:
+                        pyxel.blt(x * 8, y * 8, 0, *button_mapping[tile], 8, 8, 0)
+                    elif self.button_state[button_index] == 1:
+                        pyxel.blt(x * 8, y * 8 + 1, 0, *button_mapping[tile], 8, 7, 0)
+                    elif self.button_state[button_index] == 2:
+                        pyxel.blt(x * 8, y * 8 + self.button_state[button_index], 0, *button_mapping[tile], 8, 6, 0)
+
+                elif tile in key_mapping:
+                    key_index = [19, 22, 25].index(tile)
+
+                    if self.keys[key_index] == 1:
+                        pyxel.blt(x * 8, y * 8, 0, *key_mapping[tile], 8, 8, 0)
+                    else:
+                        pyxel.blt(x * 8, y * 8, 0, 0, 0, 8, 8, 0)
